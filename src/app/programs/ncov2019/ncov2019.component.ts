7b615583372b4efc3f4d666ad9ef328f5a5c120a
peterhu0929 <peterhu0929>
2020-02-27 18:18:33 +0800

    dynamic generate UI test

--- src/app/programs/ncov2019/ncov2019.component.ts
+++ src/app/programs/ncov2019/ncov2019.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProgramsService } from '../programs.service';
 import { ShareDialogService } from 'src/app/share-dialog/share-dialog.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
 
 
 @Component({
@@ -11,12 +12,18 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class Ncov2019Component implements OnInit {
   public ncov2019Data: Array<any>;
+  public mdCard: string;
+  public showYN = false;
+  public item = 'test';
   constructor(private programService: ProgramsService,
-    private shareDialogService: ShareDialogService) {
+    private shareDialogService: ShareDialogService,
+    private sanitizer: DomSanitizer) {
   }
 
   ngOnInit() {
     // this.getnCov2019Data();
+    // this.getCard();
+    // this.getShow(this.showYN);
   }
   getnCov2019Data() {
     this.programService.getnCov2019Data().subscribe(
@@ -26,5 +33,14 @@ export class Ncov2019Component implements OnInit {
       },
       (error: HttpErrorResponse) => this.programService.HandleError(error)
     );
+  }
+  getCard(pItem: any) {
+    const a = `<button mat-raised-button>${pItem}</button>`;
+    // const a = pItem.replace(pItem, `<button mat-raised-button color='warn'>${pItem}</button>`);
+    // console.log(this.sanitizer.bypassSecurityTrustHtml(a));
+    return this.sanitizer.bypassSecurityTrustHtml(a);
+  }
+  getShow(pBol) {
+    return !pBol;
   }
 }
